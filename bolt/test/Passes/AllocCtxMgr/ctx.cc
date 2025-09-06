3e7ecca0b5c08582b10414f8d9db25f2f248e272
lqw@128C <lqw332664203@mail.ustc.edu.cn>
2025-09-06 23:05:07 +0800

    change stub.c to stub.cc, add new_wrapper

--- bolt/test/Passes/AllocCtxMgr/ctx.cc
+++ bolt/test/Passes/AllocCtxMgr/ctx.cc
@@ -3,16 +3,25 @@
 #include <stdlib.h>
 #include <stddef.h>
 
+THREAD_LOCAL size_t TLS_GRP = 0;
+
 #ifdef __cplusplus
 extern "C" {
 #endif
 
+/*
+TODO:
+    1. bolt rewrite pass
+    2. make_shared primitive test case
+*/
 void alloc_enter(size_t grp) {
     printf("alloc enter %ld\n", grp);
+    TLS_GRP = grp;
 }
 
 void alloc_exit(size_t grp) {
     printf("alloc exit %ld\n", grp);
+    TLS_GRP = TLS_NOGRP;
 }
 
 void* malloc_wrapper(size_t size, size_t grp) {
@@ -20,6 +29,10 @@ void* malloc_wrapper(size_t size, size_t grp) {
     void* ptr = malloc(size);
     alloc_exit(grp);
     return ptr;
+}
+
+void* new_wrapper(size_t size, size_t grp) {
+    return malloc_wrapper(size, grp);
 }
 
 #ifdef __cplusplus
--- bolt/test/Passes/AllocCtxMgr/ctx.h
+++ bolt/test/Passes/AllocCtxMgr/ctx.h
@@ -2,6 +2,11 @@
 #define ALLOC_CTX_H
 #include <stddef.h>
 
+#define THREAD_LOCAL __thread
+
+extern THREAD_LOCAL size_t TLS_GRP;
+const size_t TLS_NOGRP = 0; // No Group signal
+
 #ifdef __cplusplus
 extern "C" {
 #endif
@@ -9,6 +14,7 @@ extern "C" {
 void alloc_enter(size_t grp);
 void alloc_exit(size_t grp);
 void* malloc_wrapper(size_t size, size_t grp);
+void* new_wrapper(size_t size, size_t grp);
 
 #ifdef __cplusplus
 }
