20e47c92a10ffcdab21e373f65e426b8d7a6a4ba
lqw@128C <lqw332664203@mail.ustc.edu.cn>
2025-09-06 20:43:23 +0800

    chore: make build faster
    use the following options to make building faster
        -DLLVM_CCACHE_BUILD=ON \
        -DLLVM_OPTIMIZED_TABLEGEN=ON \
        -DLLVM_USE_LINKER=mold

--- scripts/llvm-debug.sh
+++ scripts/llvm-debug.sh
@@ -1,11 +1,14 @@
 #!/bin/bash -v
+# use `find /usr -name plugin-api.h` to get the directory, then you will get LLVMgold.so
+# see https://discourse.llvm.org/t/llvmgold-so-error-loading-plugin-cannot-open-shared-object-file-no-such-file-or-directory/86858
+    # -DLLVM_BINUTILS_INCDIR=/usr/local/gcc-10/gcc-10.3.0/include \
 cd ../
 cmake -S llvm -B build -G Ninja \
     -DCMAKE_BUILD_TYPE=Debug \
-    # use `find /usr -name plugin-api.h` to get the directory, then you will get LLVMgold.so
-    # see https://discourse.llvm.org/t/llvmgold-so-error-loading-plugin-cannot-open-shared-object-file-no-such-file-or-directory/86858
-    -DLLVM_BINUTILS_INCDIR=/usr/local/gcc-10/gcc-10.3.0/include \
     -DLLVM_ENABLE_PROJECTS="clang;lld;bolt" \
     -DLLVM_TARGETS_TO_BUILD="X86;AArch64" \
-    -DLLVM_ENABLE_ASSERTIONS=ON
+    -DLLVM_ENABLE_ASSERTIONS=ON \
+    -DLLVM_CCACHE_BUILD=ON \
+    -DLLVM_OPTIMIZED_TABLEGEN=ON \
+    -DLLVM_USE_LINKER=mold 
 ./scripts/build.sh
